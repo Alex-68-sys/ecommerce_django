dbb3eff020d5a8b5916756bef6d84d0d666210eb
Alessandro <alessandro.bardossi@stud.unifi.it>
2025-07-07 15:06:24 +0200

    adattamento per railway

--- ecommerce_project/settings.py
+++ ecommerce_project/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
+
+
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -89,10 +92,11 @@ WSGI_APPLICATION = 'ecommerce_project.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL'),  # <- usato su Railway
+        conn_max_age=600,
+        conn_health_checks=True
+    )
 }
 
 
