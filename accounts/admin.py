88e0d91c31fbc11302f806b4d41569ca8d64fb17
Alessandro <alessandro.bardossi@stud.unifi.it>
2025-07-08 06:03:16 +0200

    new

--- accounts/admin.py
+++ accounts/admin.py
@@ -5,7 +5,8 @@ from .models import CustomUser
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
     fieldsets = UserAdmin.fieldsets + (
-        ('Extra Info', {'fields': ('address', 'is_store_manager')}),
+        
+        ('Extra Info', {'fields': ('address', 'is_store_manager')})
     )
 
 admin.site.register(CustomUser, CustomUserAdmin)
