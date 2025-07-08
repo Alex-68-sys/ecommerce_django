c745bc4ae4c9d19b9c5f06ef3992c4e2333db7e2
Alessandro <alessandro.bardossi@stud.unifi.it>
2025-07-08 11:54:22 +0200

    update admin.py

--- accounts/admin.py
+++ accounts/admin.py
@@ -3,10 +3,11 @@ from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 
 class CustomUserAdmin(UserAdmin):
-    model = CustomUser
-    fieldsets = UserAdmin.fieldsets + (
-        
-        ('Extra Info', {'fields': ('address', 'is_store_manager')})
-    )
+       model = CustomUser
+       fieldsets = UserAdmin.fieldsets + (
+        ('Extra Info', {
+            'fields': ('address', 'is_store_manager'),
+        }),
+    ) 
 
 admin.site.register(CustomUser, CustomUserAdmin)
