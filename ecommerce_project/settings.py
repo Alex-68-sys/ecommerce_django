985a4c3aceb62db32f3a10256f03b7055ed15743
Alessandro <alessandro.bardossi@stud.unifi.it>
2025-07-08 16:28:05 +0200

    update settings.py

--- ecommerce_project/settings.py
+++ ecommerce_project/settings.py
@@ -140,3 +140,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ALLOWED_HOSTS = ['.railway.app', '127.0.0.1', 'localhost']
+
+
+
